2faab26d5abe44e96cc5e3e8e713c970a50008ca
jhaboic <josiphaboic@gmail.com>
2021-09-02 16:55:51 +0200

    move tests to tests folder

--- src/lib.rs
+++ src/lib.rs
@@ -26,13 +26,6 @@ pub fn repr(byte: u8) -> [char; 10] {
     array
 }
 
-#[test]
-fn test_repr() {
-    let x: u8 = 0b1000_0010;
-
-    assert_eq!(['0', 'b', '1', '0', '0', '0', '0', '0', '1', '0'], repr(x));
-}
-
 pub mod bit {
     //! Bit manipulations
 
@@ -137,82 +130,5 @@ pub mod bit {
             1 => '1',
             _ => panic!("Expected 0 or 1"),
         }
-    }
-
-    #[test]
-    fn test_set() {
-        let mut x: u8 = 0b0000_0000;
-
-        set(&mut x, 0);
-        assert_eq!(x, 0b0000_0001);
-
-        set(&mut x, 7);
-        assert_eq!(x, 0b1000_0001);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_set_fail() {
-        let mut x: u8 = 0b0000_0000;
-
-        set(&mut x, 10);
-    }
-
-    #[test]
-    fn test_unset() {
-        let mut x: u8 = 0b1111_1111;
-
-        unset(&mut x, 0);
-        assert_eq!(x, 0b1111_1110);
-
-        unset(&mut x, 7);
-        assert_eq!(x, 0b0111_1110);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_unset_fail() {
-        let mut x: u8 = 0b0000_0001;
-
-        unset(&mut x, 10);
-    }
-
-    #[test]
-    fn test_toggle() {
-        let mut x: u8 = 0b1000_0001;
-
-        toggle(&mut x, 0);
-        assert_eq!(x, 0b1000_0000);
-
-        toggle(&mut x, 7);
-        assert_eq!(x, 0b0000_0000);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_toggle_fail() {
-        let mut x: u8 = 0b0000_0001;
-        toggle(&mut x, 10);
-    }
-
-    #[test]
-    fn test_as_char() {
-        let x: u8 = 0b1000_0001;
-        let mut y: char = as_char(x, 0);
-
-        assert_eq!(y, '1');
-
-        y = as_char(x, 1);
-        assert_eq!(y, '0');
-
-        y = as_char(x, 7);
-        assert_eq!(y, '1');
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_as_char_fail() {
-        let x: u8 = 0b0000_0001;
-        as_char(x, 10);
     }
 }
